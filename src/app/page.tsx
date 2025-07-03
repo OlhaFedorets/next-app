b237c861569877280a62e58495f24094ceb08820
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-03 17:32:00 +0300

    change app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
 
   return (
     <div>
-        <Link className="text-blue-800" href="/about">Перейти на страницу о нас</Link>
-        <br />
-        <Link className="text-red-600" href="/dashboard">Перейти в панель управления</Link>
+
     </div>
   );
 }
