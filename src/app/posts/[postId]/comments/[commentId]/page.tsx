9bdeea4aa7fcb2902b8b21c52a31a5583e57e26b
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-03 17:31:53 +0300

    change comments

---
+++ src/app/posts/[postId]/comments/[commentId]/page.tsx
@@ -0,0 +1,17 @@
+export default async function CommentPage({
+                                              params
+                                          }: {
+    params: Promise<{ postId: string; commentId: string }>
+}) {
+    const {postId, commentId} = await params;
+    const comment = await fetch(
+        `https://jsonplaceholder.typicode.com/comments/${commentId}?postId=${postId}`
+    ).then(res => res.json());
+    return (
+        <div>
+            <h1 className="text-2x1">Comment # {comment.id}</h1>
+            <p>{comment.body}</p>
+            <p className="text-amber-600">Author: {comment.email}</p>
+        </div>
+    )
+}
