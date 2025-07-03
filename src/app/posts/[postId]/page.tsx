48e322917ae805b29834545237be7598fe57a599
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-03 17:31:43 +0300

    change posts

---
+++ src/app/posts/[postId]/page.tsx
@@ -0,0 +1,8 @@
+export default async function PostPage({params}: { params: Promise<{ postId: string }> }) {
+    const {postId} = await params;
+    const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`).then(res => res.json());
+    return <div>
+        <h1 className="text-2x1">{post.title}</h1>
+        <p>{post.body}</p>
+    </div>
+}
