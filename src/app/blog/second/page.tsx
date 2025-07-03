63afefdb0defbb8a52e0aadc7733740b79044c12
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-03 17:30:49 +0300

    change blog

---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,8 @@
+export default function SecondBlog() {
+
+    return (
+        <h1>
+            Second post
+        </h1>
+    );
+}
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-const Button = () => {
-    return <button
-        className="bg-yellow-500"
-        onClick={()=>alert('Hello')}>Click me</button>
-}
-
-export default Button
