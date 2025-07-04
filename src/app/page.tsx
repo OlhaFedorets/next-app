d0df69f93711d4d7bda364aa3b8947d64a0afb33
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-04 14:32:05 +0300

    change app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
 
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div>
+            <h1>Главная страница</h1>
+            <nav>
+                <h2>Документация</h2>
+                <ul>
+                    <li>
+                        <Link href="/docs">Общий раздел</Link>
+                    </li>
+                    <li>
+                        <Link href="/docs/react">Реакт</Link>
+                    </li>
+                    <li>
+                        <Link href="/docs/react/hooks">Хуки</Link>
+                    </li>
+                    <li>
+                        <Link href="docs/react/hooks/useState">Хук useState</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
