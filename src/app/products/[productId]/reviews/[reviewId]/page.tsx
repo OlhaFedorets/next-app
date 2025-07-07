bfeabfbd66bbfb340f520e6c9030b16b3ec4d765
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-07 10:49:59 +0300

    change review

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,11 @@
+import {notFound} from "next/navigation";
+
 export default async function ProductReview({params}: { params: Promise<{ productId: string; reviewId: string }> }) {
     const {productId, reviewId} = await params;
+
+    if (parseInt(reviewId) > 1000) {
+        notFound()
+    }
 
     return <div>
         <h1 className="text-2x1">Review {reviewId} for product {productId}</h1>
