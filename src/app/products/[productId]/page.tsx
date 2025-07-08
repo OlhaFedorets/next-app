4fe4e1e59c525dba9e971d7bbdad1ab26c909007
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-08 12:10:49 +0300

    change productId

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,30 @@
-export default async function ProductDetails({params}: {params: Promise<{productId: string}>}) {
+import type {Metadata} from "next";
+
+type Props = {
+    params: Promise<{productId: string}>
+}
+
+export const generateMetadata = async ({
+                                           params
+}: Props): Promise<Metadata> => {
+    const id = (await params).productId
+
+    return {
+        title: `Продукт ${id}`,
+        description: `Продукт ${id} - хит продаж`,
+    }
+}
+
+export default async function ProductDetails({params}: Props) {
 const productId = (await params).productId
     return <>
         <h1>Product description {productId}</h1>
     </>
 }
+
+// export default async function ProductDetails({params}: {params: Promise<{productId: string}>}) {
+// const productId = (await params).productId
+//     return <>
+//         <h1>Product description {productId}</h1>
+//     </>
+// }
