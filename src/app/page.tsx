8fc1e98c0d4a5af3c06c73b8387e9bac3537cbae
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-08 12:10:56 +0300

    change app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,48 @@ import Link from "next/link";
 export default function Home() {
 
     return (
-        <div>
-            <h1>Главная страница</h1>
-            <nav>
-                <h2>Документация</h2>
-                <ul>
-                    <li>
-                        <Link href="/docs">Общий раздел</Link>
-                    </li>
-                    <li>
-                        <Link href="/docs/react">Реакт</Link>
-                    </li>
-                    <li>
-                        <Link href="/docs/react/hooks">Хуки</Link>
-                    </li>
-                    <li>
-                        <Link href="docs/react/hooks/useState">Хук useState</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <div>Home page</div>
+
+
+
+        // <div>
+        //     <Link href="/articles/news?languange=ru">Читать по-русски</Link>
+        //     <br/>
+        //     <Link href="/articles/news?languange=en">Читать по-английски</Link>
+        //     <br/>
+        //     <Link href="/articles/news?languange=fr">Читать по-французски</Link>
+        //     <br/>
+        //     <Link href="/articles/news ?languange=de">Читать по-немецки</Link>
+        // </div>
+
+
+
+
+
+
+
+        // <div>
+        //     <h1>Главная страница</h1>
+        //     <nav>
+        //         <h2>Документация</h2>
+        //         <ul>
+        //             <li>
+        //                 <Link href="/about">На страницу о компнаии</Link>
+        //             </li>
+        //             <li>
+        //                 <Link href="/docs">Общий раздел</Link>
+        //             </li>
+        //             <li>
+        //                 <Link href="/docs/react">Реакт</Link>
+        //             </li>
+        //             <li>
+        //                 <Link href="/docs/react/hooks">Хуки</Link>
+        //             </li>
+        //             <li>
+        //                 <Link href="docs/react/hooks/useState">Хук useState</Link>
+        //             </li>
+        //         </ul>
+        //     </nav>
+        // </div>
     );
 }
